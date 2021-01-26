d406591bc2b4edb49b0eefadb9ae6d3344b45662
Lucas Perez <lucastperez@gmail.com>
2021-01-26 01:10:41 -0300

    Usando URLs diferentes pro fundo, carregando bem mais lento...
      Por causa do problema descrito no commit 79b7b12,
    resolvi colocar 10 links, um para cada foto
    possível de fundo. Isso deixou o carregamento da
    tela bastante mais lento.

---
+++ public/bgs/urls.js
@@ -0,0 +1,16 @@
+export const bgUrls = [
+  "https://i.ibb.co/M6GspXg/1.png",
+  "https://i.ibb.co/pfzSqQP/2.png",
+  "https://i.ibb.co/kx7DHxx/3.png",
+  "https://i.ibb.co/j8p312C/4.png",
+  "https://i.ibb.co/j5C7dWj/5.png",
+  "https://i.ibb.co/YDH92rB/6.png",
+  "https://i.ibb.co/mJZ2YdK/7.png",
+  "https://i.ibb.co/syfzWnF/8.png",
+  "https://i.ibb.co/ckqdWPk/9.png",
+  "https://i.ibb.co/n8cpKpG/10.png"
+];
+
+// Versão muito pesada da foto 5 ):
+//"https://i.ibb.co/b34px2G/5.png",
+
